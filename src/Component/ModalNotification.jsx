4aa48a649c877db908636f525c9df46ac9f8efbd
pradip1232 <755201pradip@gmail.com>
2024-08-15 13:15:58 +0530

    changes time

--- src/Component/ModalNotification.jsx
+++ src/Component/ModalNotification.jsx
@@ -1,22 +1,22 @@
 // src/components/ModalNotification.jsx
-import React, { useEffect, useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { CheckCircle } from 'react-bootstrap-icons';
+import React, { useEffect, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { CheckCircle } from "react-bootstrap-icons";
 
 const ModalNotification = () => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    const hasSeenModal = localStorage.getItem('hasSeenModal');
+    const hasSeenModal = localStorage.getItem("hasSeenModal");
 
     if (!hasSeenModal) {
       setShow(true);
 
       const timer = setTimeout(() => {
         setShow(false);
-        localStorage.setItem('hasSeenModal', 'true');
-      }, 5000);
+        localStorage.setItem("hasSeenModal", "true");
+      }, 10000);
 
       return () => clearTimeout(timer);
     }
@@ -35,7 +35,9 @@ const ModalNotification = () => {
           <CheckCircle size={40} color="green" />
           <h5>Currently, I am working on this website</h5>
           <h6>Thank You For Visiting!!</h6>
-          <Button variant="primary" onClick={() => setShow(false)}>Close</Button>
+          <Button variant="primary" onClick={() => setShow(false)}>
+            Close
+          </Button>
         </div>
       </Modal.Body>
     </Modal>
